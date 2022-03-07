2c091a42c8427d92b0ade13265cb076b92fd324f
Brianna Weinstein <bweinstein23@gmail.com>
2022-03-06 23:00:33 -0500

    working rn

--- scenarios/setup/run-ansible-scripts.py
+++ scenarios/setup/run-ansible-scripts.py
@@ -4,13 +4,29 @@ import argparse, json
 # ----------------------------------------------------------
 # Arg Parsing
 # ----------------------------------------------------------
-parser = argparse.ArgumentParser(description='Specify the scenario JSON file')
-parser.add_argument('-i', dest='scenario_json_file', help='The scenario JSON file to parse')
+parser = argparse.ArgumentParser(description="Specify the scenario JSON file")
+parser.add_argument("-i", dest="scenario_json_file", help="The scenario JSON file to parse")
 args = parser.parse_args()
 # ----------------------------------------------------------
 
 # Open Scenario JSON File
-with open(args.scenario_json_file, 'r') as f:
+with open(args.scenario_json_file, "r") as f:
   json_data = json.load(f)
 
-print(json_data['linux1'])
+# Iterate through the VMs and determine which scripts to run
+for vm_name in list(json_data.keys()):
+  # Current VM data
+  vm_data = json_data[vm_name]
+  # List of scripts to run on the VM
+  vm_scripts = []
+
+  # Determine which scripts to run
+  for script in list(vm_data["scripts"].keys()):
+    if vm_data["scripts"][script]:
+      vm_scripts.append(script)
+
+  print(vm_scripts)
+
+
+
+
